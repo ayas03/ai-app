1a811bc357aae4ade49ef3bddf9ddf262cdf6db4
Ayas <aserik1021@gmail.com>
2025-03-28 22:17:23 +0500

    footer,header,navlink completed

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,22 +1,37 @@
-import Link from "next/link";
 import { FileText } from "lucide-react";
+import Navlink from "./nav-link";
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
+  const isLoggedIn = false;
   return (
     <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
       <div className="flex lg:flex-1">
-        <Link href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
+        <Navlink href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
           <FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
-          My-app
-        </Link>
+          <span className="font-extrabold lg:text-xl text-gray-900">
+            My-app
+          </span>
+        </Navlink>
       </div>
 
-      <div>
-        <Link href="/#pricing">Pricing</Link>
+      <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
+        <Navlink href="/#pricing">Pricing</Navlink>
+        {isLoggedIn && <Navlink href="/dashboard">Your summaries</Navlink>}
       </div>
 
-      <div>
-        <Link href="/#sign-in">Sign in</Link>
+      <div className="flex lg:justify-end lg:flex-1">
+        {isLoggedIn ? (
+          <div className="flex gap-2 items-center">
+            <Navlink href="/upload">Upload a PDF</Navlink>
+            <div>Pro</div>
+            <Button>Sign out</Button>
+          </div>
+        ) : (
+          <div>
+            <Navlink href="/sign-in">Sign in</Navlink>
+          </div>
+        )}
       </div>
     </nav>
   );
---
+++ components/common/nav-link.tsx
@@ -0,0 +1,31 @@
+"use client";
+import * as React from "react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+export default function Navlink({
+  href,
+  children,
+  className,
+}: {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+}) {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (href !== "/" && pathname.startsWith(href));
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "transition-colors text-sm duration-200 text-gray-600 hover:text-rose-500",
+        className,
+        isActive && "text-rose-500"
+      )}
+    >
+      {children}
+    </Link>
+  );
+}
